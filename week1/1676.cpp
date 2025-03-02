19fede7ba2de733ded62d2fe87ba1e64e4c2c52c
Wooki <hyunook02@naver.com>
2025-03-02 16:19:10 +0900

    1676: 팩토리얼 0의 개수

--- week1/1676.cpp
+++ week1/1676.cpp
@@ -2,21 +2,13 @@
 #include <string>
 using namespace std;
 
-long long fact(long long x) {
-    if (x < 2)
-        return x;
-    return x * fact(x - 1);
-}
-
 int main() {
     int n, result = 0;
     cin >> n;
 
-    string x = to_string(fact((long long) n));
-    for (int i = x.length() - 1; i >= 0; --i) {
-        if (x[i] != '0')
-            break;
-        result++;
+    while (n) {
+        result += n / 5;
+        n /= 5;
     }
 
     cout << result;
